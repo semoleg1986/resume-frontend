40006e93a3980a4f050dae31fc7218c5fa0ac533
semoleg1986 <semoleg1986@gmail.com>
2023-09-26 23:38:38 +0300

    feat: 404 (Tue, Sep 26, 2023 11:38:38 PM)

--- src/pages/nonpage.tsx
+++ src/pages/nonpage.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom'
+import { ROOT_PAGE } from '../router';
+
+
+
+const NonePage = styled.div`
+    height: 100vh;
+    background: #0f0f0f;
+    padding: 4rem;
+    text-align: center;
+`;
+const NonePageText = styled.h1`
+    font-size: 10rem;
+    color: #fff;
+`;
+
+const NonePageLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+  &:hover {
+    color: #10a710;
+  }
+`
+
 function NotFoundPage() {
-  return <div>404</div>;
+  return (
+    <NonePage>
+      <NonePageText>404</NonePageText>
+      <NonePageLink to={ROOT_PAGE}>Back to Home</NonePageLink>
+    </NonePage>
+  );
 }
 
 export default NotFoundPage;
